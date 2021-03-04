c8d7c022b77062fde1e26ca1945e2c5431d8ed70
RocketMan2 <rocketman2@iMac-RocketMan.local>
2021-03-04 16:01:09 +0600

    feat(builder): make builder pattern by example

--- index.php
+++ index.php
@@ -7,7 +7,24 @@ use Application\factory_method\facebook\FacebookPoster;
 
 
 $app = new Application();
+
+/**
+ * Фабричный метод
+ * Отлично подходит для создания разных обеъктов со схожим функционалом
+**/
 echo 'Test factory method' . "<br/>";
 $app->factoryMethod(new LinkedInPoster("kekCheburek", '123'));
 echo 'Test factory method #2' . "<br/>";
 $app->factoryMethod(new FacebookPoster("Тоже самое, но фейсбук", '123'));
+
+/**
+ * Строитель(Builder)
+ * В PHP активно используется для составление Sql запросов
+ **/
+
+echo "<br/>";
+echo 'Test MySQL query builder' . "<br/>";
+$app->builder(new \Application\builder\components\MysqlQueryBuilder());
+echo "<br/>" . "<br/>";
+echo 'Text PostreSQL query builder' . "<br/>";
+$app->builder(new \Application\builder\components\PostgresQueryBuilder());
--- src/Application.php
+++ src/Application.php
@@ -2,12 +2,22 @@
 
 namespace Application;
 
+use Application\builder\SqlQueryBuilder;
 use Application\factory_method\SocialNetworkPoster;
 
 class Application
 {
-    public function factoryMethod(SocialNetworkPoster $creator) {
+    public function factoryMethod(SocialNetworkPoster $creator): void {
         $creator->post("Hello world");
         $creator->post("I'm using factory method");
+    }
+
+    public function builder(SqlQueryBuilder $builder) {
+        echo $builder->select('users', ['*'])
+                ->where('age', 19, ">")
+                ->where('age', 30, "<")
+                ->where('email', '%rocketfirm%' ,'LIKE')
+                ->limit(10, 20)
+                ->getSql();
     }
 }
